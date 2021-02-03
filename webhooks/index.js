df92039785e07a143902f798933786d498499710
ajoe <joecora@live.com>
2021-02-03 16:20:27 +0800

    change hooks

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,54 +1,31 @@
-const http = require('http');
-const shell = require('shelljs');
-const createHandler = require('github-webhook-handler');
+var http = require('http')
+var shell = require('shelljs');
 
-var handlerOpts = [{
-  path: '/app1',
-  secret: 'secret1'
-}, {
-  path: '/app2',
-  secret: 'secret2'
-}];
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/testRepo', secret: 'secret1' })
 
-function generaterHandler(handlerOpts) {
-  var handlers = handlerOpts.reduce(function(hs, opts) {
-      hs[opts.path] = createHandler(opts)
-      return hs
-  }, {});
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(3336)
 
-  return http.createServer(function(req, res) {
-      var handler = handlers[req.url];
-      handler && handler(req, res, function(err) {
-        res.statusCode = 404
-        res.end('no such location')
-      })
-  }).listen(3333);
-}
-
-var handler = generaterHandler(handlerOpts);
-
-handler.on('error', function(err) {
-  console.error('Error:', err.message);
-});
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
 
 handler.on('push', function (event) {
-  const repository = event.payload.repository.name;
-  const action = event.payload.action;
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+    shell.exec('./scripts/deploy.sh');
+})
 
-  console.log('Received a push for %s to %s', repository, action);
-
-  var url = event.url
-  switch (url) {
-    // be careful if you use query to distinguish your app, url contains the querys, otherwise, it is equal to the path
-    case '/ajoe':
-      shell.cd('~/prodsite');
-      shell.chmod('+x', '~/prodsite/webhooks/scripts/');
-      shell.exec('~/prodsite/webhooks/scripts/deploy.sh');
-      break
-    case '/testRepo':
-      shell.exec('./scripts/deploy.sh');
-      break
-    default:
-      break
-  }
-});
+handler.on('issues', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})    
--- webhooks/scripts/deploy.sh
+++ webhooks/scripts/deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "Deploying  testRepo"
+echo "Deploying..."
 
-cd ~//prodsite \
-&& git checkout main \
-&& git pull \
-&& echo "ajoe deployed successfully"
+cd ~//prodsite
+
+if git rev-parse --git-dir > /dev/null 2>&1; then
+  echo "git initialized already"
+else
+  git init && git remote add origin git@github.com:Ajoe88/testRepo.git
+fi
+
+git fetch --all && \
+git checkout --force "origin/main"
+
+echo "Deployed successfully"
